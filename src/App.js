0f40f55ce67fc16d6b8a8aa0231251fef78b3309
Bernard Lyu <breezelv1999@gmail.com>
2019-06-17 21:24:28 +0800

    add face recognition img field and import Clarifai app

--- src/App.js
+++ src/App.js
@@ -4,60 +4,99 @@ import Navigation from './Components/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkFrom from './Components/ImageLinkFrom/ImageLinkFrom';
 import Rank from './Components/Rank';
+import FaceRecognition from './Components/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
-function App() {
-  const particleConfig = {
-          particles: {
-            number : {
-              value : 50,
-              density : {
-                enable : true,
-                value_area : 250
-              }
-            }
+const particleConfig = {
+      particles: {
+        number : {
+          value : 50,
+          density : {
+            enable : true,
+            value_area : 250
+          }
+        }
+      },
+      interactivity: {
+          detect_on: "window",
+          events: {
+            onhover: {
+              enable: true,
+              mode: 'repulse'
+            },
+            onclick: {
+              enable: true,
+              mode: "push"
+            },
+            resize:true
           },
-          interactivity: {
-              detect_on: "window",
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: 'repulse'
-                },
-                onclick: {
-                  enable: true,
-                  mode: "push"
-                },
-                resize:true
-              },
-              modes: {
-                grab: {
-                  distance: 200,
-                  line_linked: {
-                    opacity: 1
-                  }
-                },
-                repulse: {
-                  distance: 100,
-                  duration: 0.4
-                },
-                push: {
-                  particles_nb: 4
-                }
+          modes: {
+            grab: {
+              distance: 200,
+              line_linked: {
+                opacity: 1
               }
+            },
+            repulse: {
+              distance: 100,
+              duration: 0.4
+            },
+            push: {
+              particles_nb: 4
             }
-        };
-  return (
-    <div className="App">
-      <Particles className='particles' params={particleConfig}/>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkFrom />
-      {/*
-      <FaceRecognition />*/}
-    </div>
-  );
+          }
+        }
+      };
+
+const clarifai = new Clarifai.App({
+  apiKey: "1c7cb4f2b4e14d7f997f2057f1bd7966"
+});
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input : "",
+      imageUrl : ""
+    };
+  }
+
+  onInputChanged = (event) => {
+    const val = event.target.value;
+    // console.log(val);
+    this.setState({input:val});
+  }
+
+  onButtonDetect = (event) => {
+    //https://samples.clarifai.com/face-det.jpg
+    this.setState({imageUrl:this.state.input});
+    clarifai.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(
+      function(response) {
+        // do something with response
+        console.log(response.outputs[0].data.regions);
+      },
+      function(err) {
+        // there was an error
+        console.log(err);
+      }
+    )
+    .catch(console.log);
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles' params={particleConfig}/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkFrom onInputChanged={this.onInputChanged} onButtonDetect={this.onButtonDetect}/>
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+      </div>
+    );
+  } 
 }
 
 export default App;
--- src/Components/ImageLinkFrom/ImageLinkFrom.js
+++ src/Components/ImageLinkFrom/ImageLinkFrom.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-function ImageLinkForm() {
+function ImageLinkForm({onInputChanged,onButtonDetect}) {
   return (
     <div>
     	<p className='f3'>
@@ -9,8 +9,8 @@ function ImageLinkForm() {
     	</p>
     	<div className='center'>
     		<div className='form center pa4 br3 shadow-5'>
-	    		<input className='f4 pa2 w-70-ns center-ns' type='text'/>
-	    		<button className='w-30-ns f4 grow link ph3 pv2 dib white bg-blue'>Detect</button>
+	    		<input className='f4 pa2 w-70-ns center-ns' type='text' onChange={onInputChanged}/>
+	    		<button className='w-30-ns f4 grow link ph3 pv2 dib white bg-blue' onClick={onButtonDetect}>Detect</button>
 	    	</div>
     	</div>
     </div>
