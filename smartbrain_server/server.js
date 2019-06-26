f8619a4a4f2aa640b0ec5d23062ac922bc197bd0
Bernard Lyu <breezelv1999@gmail.com>
2019-06-26 20:19:59 +0800

    add signin & register route

--- smartbrain_server/server.js
+++ smartbrain_server/server.js
@@ -1,9 +1,57 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const database = {
+	users: [
+		{
+			id: '1',
+			name: 'Bernard',
+			email: 'bernard@gmail.com',
+			password: 'bernard666',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+};
+
+
+app.use(bodyParser.json());
+
+
+//Route Root
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send(database.users);
+});
+
+//Route Signin
+app.post('/signin', (req, res) => {
+	for(user of database.users) {
+		if(req.body.email===user.email && req.body.password===user.password) {
+			res.json('success');
+			return;
+		}
+	}
+	res.status(400).json("error logging in");
+});
+
+//Route Register
+app.post('/register', (req, res) => {
+	const {name, email, password} = req.body;
+	if(name&&email&&password&&email.includes('@')) {
+		database.users.push({
+			id: database.users.length + 1,
+			name: name,
+			email: email,
+			password: password,
+			entries: 0,
+			joined: new Date()
+		});
+		// res.json('success');
+		res.json(database.users);
+	}
+	else res.status(400).json("invalid registration form");
 });
 
 app.listen(3000);
